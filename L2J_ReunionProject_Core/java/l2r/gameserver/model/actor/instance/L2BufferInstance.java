2c51fec33033cf8e767a4a59e229a2642ee418dd
vGodFather <vGodFather@57f5cc2d-b006-401f-aca0-fc3285970d73>
2013-05-10 20:06:28 +0000

    Improve Buffer

--- L2J_ReunionProject_Core/java/l2r/gameserver/model/actor/instance/L2BufferInstance.java
+++ L2J_ReunionProject_Core/java/l2r/gameserver/model/actor/instance/L2BufferInstance.java
@@ -1,6 +1,5 @@
 package l2r.gameserver.model.actor.instance;
 
-import javolution.text.TextBuilder;
 import javolution.util.FastList;
 import l2r.gameserver.ThreadPoolManager;
 import l2r.gameserver.datatables.SkillTable;
@@ -14,21 +13,22 @@ import l2r.gameserver.network.SystemMessageId;
 import l2r.gameserver.network.serverpackets.ActionFailed;
 import l2r.gameserver.network.serverpackets.NpcHtmlMessage;
 import l2r.gameserver.network.serverpackets.SystemMessage;
-import gr.reunion.buffItem.AutoBuff;
-import gr.reunion.buffItem.BuffCategories;
-import gr.reunion.buffItem.BuffInstance;
-import gr.reunion.buffItem.runnable.BuffItemDelay;
 import gr.reunion.configs.AioBufferConfigs;
 import gr.reunion.datatables.CustomTable;
+import gr.reunion.javaBuffer.AutoBuff;
+import gr.reunion.javaBuffer.BuffCategories;
+import gr.reunion.javaBuffer.BuffInstance;
+import gr.reunion.javaBuffer.buffItem.runnable.BuffItemDelay;
+import gr.reunion.javaBuffer.buffNpc.dynamicHtmls.GenerateHtmls;
+import gr.reunion.javaBuffer.buffNpc.dynamicHtmls.GenerateHtmls.Packet;
+import gr.reunion.javaBuffer.buffNpc.runnable.BuffNpcDeleter;
+import gr.reunion.javaBuffer.buffNpc.runnable.BuffNpcSaver;
 import gr.reunion.main.Conditions;
 import gr.reunion.securitySystem.SecurityActions;
 import gr.reunion.securitySystem.SecurityType;
 
 public class L2BufferInstance extends L2Npc
 {
-	/* Constants for dynamic htmls */
-	private static final String _headHtml = "<html><title>Npc Buffer</title><body><center>";
-	private static final String _endHtml = "</center></body></html>";
 	/* The coin used to buff players */
 	private static final int _coinperbuff = AioBufferConfigs.AIO_BUFFCOIN;
 	/* Price per buff */
@@ -36,34 +36,6 @@ public class L2BufferInstance extends L2Npc
 	/* buffs amount */
 	private static final int _maxDance = AioBufferConfigs.AIO_MAXDANCE_PERPROFILE;
 	private static final int _maxBuffs = AioBufferConfigs.AIO_MAXBUFFS_PERPROFILE;
-	
-	public enum Packet
-	{
-		DYNAMIC,
-		FILE
-	}
-	
-	/**
-	 * Method to send the html to char
-	 * @param player
-	 * @param html
-	 * @param packet
-	 */
-	private void sendPacket(L2PcInstance player, String html, Packet packet)
-	{
-		NpcHtmlMessage msg = new NpcHtmlMessage(getObjectId());
-		if (packet.equals(Packet.FILE))
-		{
-			msg.setFile(player.getHtmlPrefix(), "/data/html/NpcBuffer/" + html);
-			msg.replace("%objectId%", String.valueOf(getObjectId()));
-		}
-		if (packet.equals(Packet.DYNAMIC))
-		{
-			msg.setHtml(html);
-			msg.replace("%objectId%", String.valueOf(getObjectId()));
-		}
-		player.sendPacket(msg);
-	}
 	
 	@Override
 	public void showChatWindow(L2PcInstance player)
@@ -251,13 +223,13 @@ public class L2BufferInstance extends L2Npc
 		else if (command.startsWith("removebuff"))
 		{
 			player.stopAllEffects();
-			sendPacket(player, "555-2.htm", Packet.FILE);
+			GenerateHtmls.sendPacket(player, "555-2.htm", Packet.FILE, getObjectId());
 		}
 		else if (command.startsWith("healme"))
 		{
 			player.setCurrentHpMp(player.getMaxHp(), player.getMaxMp());
 			player.setCurrentCp(player.getMaxCp());
-			sendPacket(player, "555-2.htm", Packet.FILE);
+			GenerateHtmls.sendPacket(player, "555-2.htm", Packet.FILE, getObjectId());
 		}
 		else if (command.startsWith("autobuff"))
 		{
@@ -268,7 +240,7 @@ public class L2BufferInstance extends L2Npc
 			}
 			
 			AutoBuff.autoBuff(player);
-			sendPacket(player, "555-2.htm", Packet.FILE);
+			GenerateHtmls.sendPacket(player, "555-2.htm", Packet.FILE, getObjectId());
 		}
 		else if (command.startsWith("buff"))
 		{
@@ -310,28 +282,28 @@ public class L2BufferInstance extends L2Npc
 			switch (subCommand[0])
 			{
 				case "buffdance":
-					sendPacket(player, "555-3.htm", Packet.FILE);
+					GenerateHtmls.sendPacket(player, "555-3.htm", Packet.FILE, getObjectId());
 					break;
 				case "buffsong":
-					sendPacket(player, "555-4.htm", Packet.FILE);
+					GenerateHtmls.sendPacket(player, "555-4.htm", Packet.FILE, getObjectId());
 					break;
 				case "buffprop":
-					sendPacket(player, "555-5.htm", Packet.FILE);
+					GenerateHtmls.sendPacket(player, "555-5.htm", Packet.FILE, getObjectId());
 					break;
 				case "buffover":
-					sendPacket(player, "555-6.htm", Packet.FILE);
+					GenerateHtmls.sendPacket(player, "555-6.htm", Packet.FILE, getObjectId());
 					break;
 				case "buffdwarf":
-					sendPacket(player, "555-7.htm", Packet.FILE);
+					GenerateHtmls.sendPacket(player, "555-7.htm", Packet.FILE, getObjectId());
 					break;
 				case "buffwar":
-					sendPacket(player, "555-8.htm", Packet.FILE);
+					GenerateHtmls.sendPacket(player, "555-8.htm", Packet.FILE, getObjectId());
 					break;
 				case "buffmisc":
-					sendPacket(player, "555-9.htm", Packet.FILE);
+					GenerateHtmls.sendPacket(player, "555-9.htm", Packet.FILE, getObjectId());
 					break;
 				case "buffelder":
-					sendPacket(player, "555-10.htm", Packet.FILE);
+					GenerateHtmls.sendPacket(player, "555-10.htm", Packet.FILE, getObjectId());
 					break;
 				default:
 					break;
@@ -351,39 +323,39 @@ public class L2BufferInstance extends L2Npc
 			catch (Exception e)
 			{
 				player.sendMessage("Please specify a valid profile name.");
-				sendPacket(player, "555-11.htm", Packet.FILE);
+				GenerateHtmls.sendPacket(player, "555-11.htm", Packet.FILE, getObjectId());
 				return;
 			}
 			
-			sendPacket(player, "555-13.htm", Packet.FILE);
+			GenerateHtmls.sendPacket(player, "555-13.htm", Packet.FILE, getObjectId());
 		}
 		else if (command.startsWith("showAvaliable"))
 		{
 			switch (subCommand[0])
 			{
 				case "showAvaliableDwarf":
-					showBuffsToAdd(player, subCommand[1], BuffCategories.DWARF, "addDwarf");
+					GenerateHtmls.showBuffsToAdd(player, subCommand[1], BuffCategories.DWARF, "addDwarf", getObjectId());
 					break;
 				case "showAvaliableMisc":
-					showBuffsToAdd(player, subCommand[1], BuffCategories.MISC, "addMisc");
+					GenerateHtmls.showBuffsToAdd(player, subCommand[1], BuffCategories.MISC, "addMisc", getObjectId());
 					break;
 				case "showAvaliableElder":
-					showBuffsToAdd(player, subCommand[1], BuffCategories.ELDER, "addElder");
+					GenerateHtmls.showBuffsToAdd(player, subCommand[1], BuffCategories.ELDER, "addElder", getObjectId());
 					break;
 				case "showAvaliableChant":
-					showBuffsToAdd(player, subCommand[1], BuffCategories.CHANT, "addChant");
+					GenerateHtmls.showBuffsToAdd(player, subCommand[1], BuffCategories.CHANT, "addChant", getObjectId());
 					break;
 				case "showAvaliableOver":
-					showBuffsToAdd(player, subCommand[1], BuffCategories.OVERLORD, "addOver");
+					GenerateHtmls.showBuffsToAdd(player, subCommand[1], BuffCategories.OVERLORD, "addOver", getObjectId());
 					break;
 				case "showAvaliableProp":
-					showBuffsToAdd(player, subCommand[1], BuffCategories.PROPHET, "addProp");
+					GenerateHtmls.showBuffsToAdd(player, subCommand[1], BuffCategories.PROPHET, "addProp", getObjectId());
 					break;
 				case "showAvaliableDance":
-					showBuffsToAdd(player, subCommand[1], BuffCategories.DANCE, "addDance");
+					GenerateHtmls.showBuffsToAdd(player, subCommand[1], BuffCategories.DANCE, "addDance", getObjectId());
 					break;
 				case "showAvaliableSong":
-					showBuffsToAdd(player, subCommand[1], BuffCategories.SONG, "addSong");
+					GenerateHtmls.showBuffsToAdd(player, subCommand[1], BuffCategories.SONG, "addSong", getObjectId());
 					break;
 				default:
 					break;
@@ -437,7 +409,7 @@ public class L2BufferInstance extends L2Npc
 				}
 			}
 			
-			ThreadPoolManager.getInstance().executeTask(new buffSaver(player, category, subCommand[1], Integer.parseInt(subCommand[2]), this));
+			ThreadPoolManager.getInstance().executeTask(new BuffNpcSaver(player, category, subCommand[1], Integer.parseInt(subCommand[2]), this));
 		}
 		
 		// Scheme removals
@@ -447,246 +419,16 @@ public class L2BufferInstance extends L2Npc
 		}
 		else if (command.startsWith("showBuffsToDelete"))
 		{
-			showBuffsToDelete(player, subCommand[1], "removeBuffs");
+			GenerateHtmls.showBuffsToDelete(player, subCommand[1], "removeBuffs", getObjectId());
 		}
 		else if (command.startsWith("removeBuffs"))
 		{
-			ThreadPoolManager.getInstance().executeTask(new buffDeleter(player, subCommand[1], Integer.parseInt(subCommand[2]), this));
+			ThreadPoolManager.getInstance().executeTask(new BuffNpcDeleter(player, subCommand[1], Integer.parseInt(subCommand[2]), this));
 		}
 		else if (command.startsWith("showProfiles"))
 		{
-			showSchemeToEdit(player, subCommand[1]);
+			GenerateHtmls.showSchemeToEdit(player, subCommand[1], getObjectId());
 		}
-	}
-	
-	public void callBuffToAdd(BuffCategories category, L2PcInstance player, String profile)
-	{
-		String bypass = "addProp";
-		
-		switch (category)
-		{
-			case DANCE:
-				bypass = "addDance";
-				break;
-			case SONG:
-				bypass = "addSong";
-				break;
-			case MISC:
-				bypass = "addMisc";
-				break;
-			case ELDER:
-				bypass = "addElder";
-				break;
-			case OVERLORD:
-				bypass = "addOver";
-				break;
-			case PROPHET:
-				bypass = "addProp";
-				break;
-			case DWARF:
-				bypass = "addDwarf";
-				break;
-			case CHANT:
-				bypass = "addChant";
-				break;
-			case NONE:
-				bypass = "removeBuffs";
-				break;
-			default:
-				break;
-		}
-		showBuffsToAdd(player, profile, category, bypass);
-	}
-	
-	// SCHEME SYSTEM
-	private void showSchemeToEdit(L2PcInstance player, String action)
-	{
-		FastList<String> profileNames = player.getProfiles();
-		
-		TextBuilder tb = new TextBuilder();
-		tb.append(_headHtml);
-		tb.append("Choose the profile<br></center><font color=00FFFF>Scheme Profiles:</font><center><img src=\"L2UI.SquareGray\" width=280 height=1><table bgcolor=131210>");
-		for (String profile : profileNames)
-		{
-			tb.append("<tr>");
-			tb.append("<td align=center><button value=\"" + profile + "\" action=\"bypass -h npc_%objectId%_" + action + "_" + profile + "\" width=135 height=28 back=\"L2UI_CT1.Button_DF_Down\" fore=\"L2UI_ct1.button_df\"></td>");
-			tb.append("</tr>");
-		}
-		tb.append("</table><img src=\"L2UI.SquareGray\" width=280 height=1>");
-		tb.append(_endHtml);
-		
-		sendPacket(player, tb.toString(), Packet.DYNAMIC);
-	}
-	
-	/**
-	 * Shows the available buffs to add to a profile
-	 * @param player
-	 * @param profile
-	 * @param category
-	 * @param bypass
-	 */
-	public void showBuffsToAdd(L2PcInstance player, String profile, BuffCategories category, String bypass)
-	{
-		FastList<Integer> ownedBuffs = player.getProfileBuffs(profile);
-		int i = 0;
-		
-		TextBuilder tb = new TextBuilder();
-		tb.append(_headHtml);
-		tb.append("Choose the buffs to be added<br></center>");
-		switch (category)
-		{
-			case CHANT:
-				tb.append("<font color=00FFFF>Scheme Buffer: Chant</font>");
-				break;
-			case DANCE:
-				tb.append("<font color=00FFFF>Scheme Buffer: Dance</font>");
-				break;
-			case SONG:
-				tb.append("<font color=00FFFF>Scheme Buffer: Song</font>");
-				break;
-			case OVERLORD:
-				tb.append("<font color=00FFFF>Scheme Buffer: Overlord</font>");
-				break;
-			case PROPHET:
-				tb.append("<font color=00FFFF>Scheme Buffer: Prophet</font>");
-				break;
-			case ELDER:
-				tb.append("<font color=00FFFF>Scheme Buffer: Elder</font>");
-				break;
-			case DWARF:
-				tb.append("<font color=00FFFF>Scheme Buffer: Dwarf</font>");
-				break;
-			case MISC:
-				tb.append("<font color=00FFFF>Scheme Buffer: Misc</font>");
-				break;
-			default:
-				break;
-		}
-		tb.append("<center><img src=\"L2UI.SquareGray\" width=280 height=1>");
-		for (BuffInstance buffInst : CustomTable.getInstance().getBuffs().values())
-		{
-			// Just a check to know if this buff
-			// Is in the category we wish to be
-			if (buffInst.getCategory() != category)
-			{
-				continue;
-			}
-			
-			int id = buffInst.getId();
-			Integer level = buffInst.getLevel();
-			String description = buffInst.getDescription();
-			String name = buffInst.getName();
-			
-			// Check if the buff id exists in the owned buffs fastlist
-			// Remember the l2pc has only the buffId contained
-			if ((ownedBuffs != null) && ownedBuffs.contains(id))
-			{
-				continue;
-			}
-			
-			if ((i % 2) == 0)
-			{
-				tb.append("<table bgcolor=131210>");
-			}
-			else
-			{
-				tb.append("<table>");
-			}
-			tb.append("<tr>");
-			if ((id == 4699) || (id == 4700))
-			{
-				tb.append("<td width=40><button action=\"bypass -h npc_%objectId%_" + bypass + "_" + profile + "_" + id + "\" width=32 height=32 back=\"icon.skill1331\" fore=\"icon.skill1331\"></td>");
-			}
-			if ((id == 4702) || (id == 4703))
-			{
-				tb.append("<td width=40><button action=\"bypass -h npc_%objectId%_" + bypass + "_" + profile + "_" + id + "\" width=32 height=32 back=\"icon.skill1332\" fore=\"icon.skill1332\"></td>");
-			}
-			if (id < 1000)
-			{
-				tb.append("<td width=40><button action=\"bypass -h npc_%objectId%_" + bypass + "_" + profile + "_" + id + "\" width=32 height=32 back=\"icon.skill0" + id + "\" fore=\"icon.skill0" + id + "\"></td>");
-			}
-			if ((id > 1000) && (id != 4699) && (id != 4700) && (id != 4702) && (id != 4703))
-			{
-				tb.append("<td width=40><button action=\"bypass -h npc_%objectId%_" + bypass + "_" + profile + "_" + id + "\" width=32 height=32 back=\"icon.skill" + id + "\" fore=\"icon.skill" + id + "\"></td>");
-			}
-			tb.append("<td><table>");
-			tb.append("<tr><td width=220>" + name + "<font color=a1a1a1> Lv</font> <font color=ae9977>" + level + "</font></td></tr>");
-			tb.append("<tr><td width=220><font color=b0bccc>" + description + "</font></td></tr></table></td></tr>");
-			tb.append("</table>");
-			i++;
-		}
-		tb.append("<br><br><img src=L2UI.SquareWhite width=280 height=1><button value=\"Back\" action=\"bypass -h npc_%objectId%_Chat_555-13.htm\" width=90 height=21 back=\"L2UI_CT1.Button_DF_Down\" fore=\"L2UI_ct1.button_df\">");
-		tb.append(_endHtml);
-		
-		sendPacket(player, tb.toString(), Packet.DYNAMIC);
-	}
-	
-	// Shows the available buffs to add to a profile;
-	public void showBuffsToDelete(L2PcInstance player, String profile, String bypass)
-	{
-		FastList<Integer> ownedBuffs = player.getProfileBuffs(profile);
-		int i = 0;
-		
-		if (ownedBuffs == null)
-		{
-			player.sendMessage("There are no buffs in that profile.");
-			return;
-		}
-		
-		TextBuilder tb = new TextBuilder();
-		tb.append(_headHtml);
-		tb.append("Choose the buffs to be deleted<br></center>");
-		tb.append("<font color=00FFFF>Scheme Buffer: Remove</font>");
-		tb.append("<center><img src=\"L2UI.SquareGray\" width=280 height=1><table bgcolor=131210>");
-		for (BuffInstance buffInst : CustomTable.getInstance().getBuffs().values())
-		{
-			int id = buffInst.getId();
-			Integer level = buffInst.getLevel();
-			String description = buffInst.getDescription();
-			String name = buffInst.getName();
-			
-			// Check if the buff id exists in the owned buffs fastlist
-			// Remember the l2pc has only the buffId contained
-			if (!ownedBuffs.contains(id))
-			{
-				continue;
-			}
-			
-			if ((i % 2) == 0)
-			{
-				tb.append("<table bgcolor=131210>");
-			}
-			else
-			{
-				tb.append("<table>");
-			}
-			tb.append("<tr>");
-			if ((id == 4699) || (id == 4700))
-			{
-				tb.append("<td width=40><button action=\"bypass -h npc_%objectId%_" + bypass + "_" + profile + "_" + id + "\" width=32 height=32 back=\"icon.skill1331\" fore=\"icon.skill1331\"></td>");
-			}
-			if ((id == 4702) || (id == 4703))
-			{
-				tb.append("<td width=40><button action=\"bypass -h npc_%objectId%_" + bypass + "_" + profile + "_" + id + "\" width=32 height=32 back=\"icon.skill1332\" fore=\"icon.skill1332\"></td>");
-			}
-			if (id < 1000)
-			{
-				tb.append("<td width=40><button action=\"bypass -h npc_%objectId%_" + bypass + "_" + profile + "_" + id + "\" width=32 height=32 back=\"icon.skill0" + id + "\" fore=\"icon.skill0" + id + "\"></td>");
-			}
-			if ((id > 1000) && (id != 4699) && (id != 4700) && (id != 4702) && (id != 4703))
-			{
-				tb.append("<td width=40><button action=\"bypass -h npc_%objectId%_" + bypass + "_" + profile + "_" + id + "\" width=32 height=32 back=\"icon.skill" + id + "\" fore=\"icon.skill" + id + "\"></td>");
-			}
-			tb.append("<td><table>");
-			tb.append("<tr><td width=220>" + name + "<font color=a1a1a1> Lv</font> <font color=ae9977>" + level + "</font></td></tr>");
-			tb.append("<tr><td width=220><font color=b0bccc>" + description + "</font></td></tr></table></td></tr>");
-			tb.append("</table>");
-			i++;
-		}
-		tb.append("<br><br><img src=L2UI.SquareWhite width=280 height=1><button value=\"Back\" action=\"bypass -h npc_%objectId%_Chat_555.htm\" width=90 height=21 back=\"L2UI_CT1.Button_DF_Down\" fore=\"L2UI_ct1.button_df\">");
-		tb.append(_endHtml);
-		
-		sendPacket(player, tb.toString(), Packet.DYNAMIC);
 	}
 	
 	private boolean checkDanceAmount(L2PcInstance player, String profile, BuffCategories category)
@@ -694,7 +436,7 @@ public class L2BufferInstance extends L2Npc
 		if (player.getDanceSongCount(profile) == _maxDance)
 		{
 			player.sendMessage("You cannot add more than " + _maxDance + " dances-songs.");
-			callBuffToAdd(category, player, profile);
+			GenerateHtmls.callBuffToAdd(category, player, profile, getObjectId());
 			return false;
 		}
 		return true;
@@ -705,7 +447,7 @@ public class L2BufferInstance extends L2Npc
 		if (player.getOtherBuffCount(profile) == _maxBuffs)
 		{
 			player.sendMessage("You cannot add more than " + _maxBuffs + " buffs.");
-			callBuffToAdd(category, player, profile);
+			GenerateHtmls.callBuffToAdd(category, player, profile, getObjectId());
 			return false;
 		}
 		return true;
