bf8f4c7cc6837218d56638c3dc0a98aaf2c16024
vGodFather <vGodFather@57f5cc2d-b006-401f-aca0-fc3285970d73>
2013-06-24 11:58:48 +0000

    Extra check before give item to pet, checking for Item manipulation

--- L2J_ReunionProject_Core/java/l2r/gameserver/network/clientpackets/RequestGiveItemToPet.java
+++ L2J_ReunionProject_Core/java/l2r/gameserver/network/clientpackets/RequestGiveItemToPet.java
@@ -134,6 +134,12 @@ public final class RequestGiveItemToPet extends L2GameClientPacket
 			return;
 		}
 		
+		// Checking for item manipulation
+		if (player.checkItemManipulation(_objectId, _amount, "GiveItemToPet") == null)
+		{
+			return;
+		}
+		
 		if (player.transferItem("Transfer", _objectId, _amount, pet.getInventory(), pet) == null)
 		{
 			_log.warning("Invalid item transfer request: " + pet.getName() + "(pet) --> " + player.getName());
