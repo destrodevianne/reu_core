f3fafd028dc1f34e4fe6475f1c91b92ae5cdcc58
vGodFather <vGodFather@57f5cc2d-b006-401f-aca0-fc3285970d73>
2013-08-11 23:06:35 +0000

    Reworking some distances checks Thanks L2j

--- trunk/L2J_ReunionProject_Core/java/l2r/gameserver/network/clientpackets/RequestActionUse.java
+++ trunk/L2J_ReunionProject_Core/java/l2r/gameserver/network/clientpackets/RequestActionUse.java
@@ -951,8 +951,8 @@ public final class RequestActionUse extends L2GameClientPacket
 			return;
 		}
 		
-		final double distance = Math.sqrt(requester.getPlanDistanceSq(target));
-		if ((distance > 2000) || (distance < 70) || (requester.getObjectId() == target.getObjectId()))
+		final int distance = (int) Math.sqrt(requester.getPlanDistanceSq(target));
+		if ((distance > 125) || (distance < 15) || (requester.getObjectId() == target.getObjectId()))
 		{
 			sendPacket(SystemMessageId.TARGET_DO_NOT_MEET_LOC_REQUIREMENTS);
 			return;
