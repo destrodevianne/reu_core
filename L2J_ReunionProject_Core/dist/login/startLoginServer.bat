2b05f1b66e3e9cb12bebe5c196fc87b0ae40f72d
vGodFather <vGodFather@57f5cc2d-b006-401f-aca0-fc3285970d73>
2013-11-16 15:45:23 +0000

    Clean up server starters Deleted not used classes

--- L2J_ReunionProject_Core/dist/login/startLoginServer.bat
+++ L2J_ReunionProject_Core/dist/login/startLoginServer.bat
@@ -5,7 +5,7 @@ color 0B
 echo Starting L2J Login Server.
 echo.
 
-java -Djava.util.logging.manager=l2r.util.L2LogManager -Xmx256m -Xbootclasspath/p:./../libs/l2ft.jar -cp config/xml;./../libs/*;l2jlogin.jar l2r.loginserver.L2LoginServer
+java -server -Dfile.encoding=UTF-8 -Xmx256m -Xbootclasspath/p:./../libs/l2ft.jar -cp config/xml;./../libs/*;l2jlogin.jar l2r.loginserver.L2LoginServer
 
 if ERRORLEVEL 2 goto restart
 if ERRORLEVEL 1 goto error
