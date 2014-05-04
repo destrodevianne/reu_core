77459416d6554abbadc331a782da972180335b22
vGodFather <vGodFather@57f5cc2d-b006-401f-aca0-fc3285970d73>
2014-05-04 22:20:58 +0000

    Reworked NonTalkingNpc script
    Moved Wedding npc from core to scripts
    
    compile and copy paste reunion-core.jar, reunion-scripts.jar in correct folders
    
    Fixed some fish issues

--- trunk/L2J_ReunionProject_Core/java/l2r/gameserver/model/actor/L2Npc.java
+++ trunk/L2J_ReunionProject_Core/java/l2r/gameserver/model/actor/L2Npc.java
@@ -137,6 +137,7 @@ public class L2Npc extends L2Character
 	private final int _minimalSocialInterval = 6000;
 	/** Support for random animation switching */
 	private boolean _isRandomAnimationEnabled = true;
+	private boolean _isTalking = true;
 	protected RandomAnimationTask _rAniTask = null;
 	private int _currentLHandId; // normally this shouldn't change from the template, but there exist exceptions
 	private int _currentRHandId; // normally this shouldn't change from the template, but there exist exceptions
@@ -1167,7 +1168,7 @@ public class L2Npc extends L2Character
 	 */
 	public void showChatWindow(L2PcInstance player, int val)
 	{
-		if (Config.NON_TALKING_NPCS.contains(getId()))
+		if (!isTalking())
 		{
 			player.sendPacket(ActionFailed.STATIC_PACKET);
 			return;
@@ -2163,6 +2164,24 @@ public class L2Npc extends L2Character
 			}
 		}
 		return super.isVisibleFor(player);
+	}
+	
+	/**
+	 * Sets if the players can talk with this npc or not
+	 * @param val {@code true} if the players can talk, {@code false} otherwise
+	 */
+	public void setTalking(boolean val)
+	{
+		_isTalking = val;
+	}
+	
+	/**
+	 * Checks if the players can talk to this npc.
+	 * @return {@code true} if the players can talk, {@code false} otherwise.
+	 */
+	public boolean isTalking()
+	{
+		return _isTalking;
 	}
 	
 	private boolean _blocked;
