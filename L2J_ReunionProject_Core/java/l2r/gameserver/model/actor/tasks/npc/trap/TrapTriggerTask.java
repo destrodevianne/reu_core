372af4b8d7a59755aed112b739950c6f0bd10afd
vGodFather <vGodFather@57f5cc2d-b006-401f-aca0-fc3285970d73>
2014-05-26 15:03:02 +0000

    Fixed trap issues.... Now all traps worked as they should be... Kamaels now are able to spawn more than one traps at the same time... Now traps when they explode they flag their owner... Cannot spawn traps in peace zones.... Fixed fire trap range... was 1100 OMG nerfed to 250 Fixed trap with hold skill range... was 1100 OMG nerfed to 250 as well
    compile and copy paste reunion-core.jar

--- L2J_ReunionProject_Core/java/l2r/gameserver/model/actor/tasks/npc/trap/TrapTriggerTask.java
+++ L2J_ReunionProject_Core/java/l2r/gameserver/model/actor/tasks/npc/trap/TrapTriggerTask.java
@@ -39,6 +39,7 @@ public class TrapTriggerTask implements Runnable
 	{
 		try
 		{
+			_trap.getOwner().updatePvPStatus();
 			_trap.doCast(_trap.getSkill());
 			ThreadPoolManager.getInstance().scheduleGeneral(new TrapUnsummonTask(_trap), _trap.getSkill().getHitTime() + 300);
 		}
